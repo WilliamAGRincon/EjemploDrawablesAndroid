083aa3195e175a0c4a37a70973481cfe02736e66
William Andres Giraldor Rincon <William Andres Giraldor Rincon>
2015-11-21 12:37:19 -0500

    Se suben últimos cambios hechos en clase

---
+++ app/src/main/java/glider/com/ejemplodrawablesandroid/splash.java
@@ -0,0 +1,15 @@
+package glider.com.ejemplodrawablesandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by andre on 21/11/2015.
+ */
+public class splash extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.splash);
+    }
+}
